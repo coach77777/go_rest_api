bb6de328235d4e4f7d04fe3e549bd4a2f0711e80
Craig Nelson <cn60@hotmail.com>
2026-04-07 08:23:33 -0500

    Refactoring Code & Outsourcing Routes

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
+	
 	"example.com/rest-api/db"
-	"example.com/rest-api/models"
+	
+	"example.com/rest-api/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,79 +12,9 @@ func main() {
 	db.InitDB()
 
 	server := gin.Default()
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
+	
+	routes.RegisterRoutes(server)
 
 	server.Run(":8080")
 }
 
-func getEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{
-			"message": "Could not retrieve events.",
-			"error":   err.Error(),
-		})
-		return
-	}
-
-	context.JSON(http.StatusOK, events)
-}
-
-func getEvent(context *gin.Context) {
-	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{
-			"message": "Invalid event ID.",
-			"error":   err.Error(),
-		})
-		return
-	}
-
-	event, err := models.GetEventByID(eventId)
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{
-			"message": "Could not retrieve event.",
-			"error":   err.Error(),
-		})
-		return
-	}
-
-	if event == nil {
-		context.JSON(http.StatusNotFound, gin.H{
-			"message": "Event not found.",
-		})
-		return
-	}
-
-	context.JSON(http.StatusOK, event)
-}
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-
-	err := context.BindJSON(&event)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{
-			"message": "Could not parse data.",
-			"error":   err.Error(),
-		})
-		return
-	}
-
-	event.UserID = 1
-	err = event.Save()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{
-			"message": "Could not save/create event.",
-			"error":   err.Error(),
-		})
-		return
-	}
-
-	context.JSON(http.StatusCreated, gin.H{
-		"message": "Event created successfully.",
-		"event":   event,
-	})
-}
---
+++ routes/routes.go
@@ -0,0 +1,9 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(server *gin.Engine) {
+	server.GET("/events", getEvents)  //Get, post, put, delete events
+	server.GET("/events/:id", getEvent) //examples of getting a single event by ID
+	server.POST("/events", createEvent) //example of creating a new event
+}
