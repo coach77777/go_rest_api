2f7540041be8edd45a6e0389577ce144a1983194
Craig Nelson <cn60@hotmail.com>
2026-04-07 08:12:43 -0500

    Getting Single Event Data By ID

--- main.go
+++ main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"net/http"
+	"strconv"
+
 	"example.com/rest-api/db"
 	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
-
-	"net/http"
 )
 
 func main() {
 	db.InitDB()
+
 	server := gin.Default()
-	server.GET("/events", getEvents) //Get,Post,Put,Delete,Patch,Head,Options,Any
+	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
-	server.Run(":8080") //localhost:8080
+	server.Run(":8080")
 }
 
 func getEvents(context *gin.Context) {
-	events,err := models.GetAllEvents()
+	events, err := models.GetAllEvents()
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Could not retrieve events.",
@@ -28,26 +31,51 @@ func getEvents(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusOK, events)
+}
 
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid event ID.",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not retrieve event.",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	if event == nil {
+		context.JSON(http.StatusNotFound, gin.H{
+			"message": "Event not found.",
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context) {
 	var event models.Event
 
 	err := context.BindJSON(&event)
-
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{
-			"message": "Could not parse Data.",
+			"message": "Could not parse data.",
 			"error":   err.Error(),
 		})
 		return
 	}
 
-	event.ID = 1
 	event.UserID = 1
 	err = event.Save()
-
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Could not save/create event.",
@@ -56,10 +84,8 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-
 	context.JSON(http.StatusCreated, gin.H{
 		"message": "Event created successfully.",
 		"event":   event,
 	})
-
 }
