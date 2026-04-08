f4481f2959f0b74afda10e6bb6e112d22810ffe3
Craig Nelson <cn60@hotmail.com>
2026-04-08 07:16:28 -0500

    Finishing The JWT Logic

--- routes/users.go
+++ routes/users.go
@@ -1,10 +1,12 @@
 package routes
 
 import (
+	
 	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"example.com/rest-api/models"
+	"example.com/rest-api/utils"
+	"github.com/gin-gonic/gin"
 )
 
 func signup(context *gin.Context) {
@@ -40,7 +42,13 @@ func login(context *gin.Context) {
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+	token, err := utils.GenerateToken(user.Email, user.ID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not generate token"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful", "token": token	})
 
 }
 
